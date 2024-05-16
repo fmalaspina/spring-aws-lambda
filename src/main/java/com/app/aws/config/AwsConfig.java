e4a3b3cc665e91b66a227b771fccc991b5a76a42
fmalaspina <f_malaspina@hotmail.com>
2024-05-16 08:25:12 -0300

    got rid of security filter

--- src/main/java/com/app/aws/config/AwsConfig.java
+++ src/main/java/com/app/aws/config/AwsConfig.java
@@ -12,29 +12,10 @@ import java.util.function.Supplier;
 @Configuration
 public class AwsConfig {
 
-    @Bean
-    public Filter getFilter() {
-        return new SecurityFilter();
-    }
-
-//    @Bean(name="stock")
-//    public Function<Map<String,String>, Map<String,Object>> getStock() {
-//        return (map) -> {
-//            var symbol = map.get("symbol");
-//            RestTemplate restTemplate = new RestTemplate();
-//            var result = restTemplate
-//                    .getForObject("https://eodhd.com/api/fundamentals/{symbol}?api_token=demo&fmt=json",Map.class, symbol);
-//            return result;
-//        };
-//    }
-
     @Bean(name="reverse")
     public Function<String,String> reverse() {
         return input -> new StringBuilder(input).reverse().toString();
     }
 
-    @Bean(name="normal")
-    public Function<String,String> normal() {
-        return input -> input;
-    }
+
 }
